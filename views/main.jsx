c380bb5e7f9031b48ad0c839c4563a2a9612d761
benzookapi <jokksk@gmail.com>
2023-01-07 20:51:01 +0900

    #

--- views/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>AAAAAAAA</h1>
-  </React.StrictMode>,
-)
