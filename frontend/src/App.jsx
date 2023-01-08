2dd3f05fd4d9c62c5bed2dc8639a1a4527e9737c
benzookapi <jokksk@gmail.com>
2023-01-08 18:45:24 +0900

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,72 @@
-import { useState } from 'react'
+import {
+  AppProvider,
+  Page,
+  Card,
+  ResourceList,
+  Avatar,
+  Text,
+} from '@shopify/polaris';
+import React from 'react';
 
+// See https://www.npmjs.com/package/@shopify/polaris
+import '@shopify/polaris/build/esm/styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <AppProvider
+      i18n={{
+        Polaris: {
+          ResourceList: {
+            sortingLabel: 'Sort by',
+            defaultItemSingular: 'item',
+            defaultItemPlural: 'items',
+            showing: 'Showing {itemsCount} {resource}',
+            Item: {
+              viewItem: 'View details for {itemName}',
+            },
+          },
+          Common: {
+            checkbox: 'checkbox',
+          },
+        },
+      }}
+    >
+      <Page>
+        <Card>
+          <ResourceList
+            showHeader
+            items={[
+              {
+                id: 341,
+                url: 'customers/341',
+                name: 'Mae Jemison',
+                location: 'AAAAADecatur, USA',
+              },
+              {
+                id: 256,
+                url: 'customers/256',
+                name: 'Ellen Ochoa',
+                location: 'Los Angeles, USA',
+              },
+            ]}
+            renderItem={(item) => {
+              const {id, url, name, location} = item;
+              const media = <Avatar customer size="medium" name={name} />;
 
-    </div>
-  )
+              return (
+                <ResourceList.Item id={id} url={url} media={media}>
+                  <Text variant="bodyMd" fontWeight="bold" as="h3">
+                    {name}
+                  </Text>
+                  <div>{location}</div>
+                </ResourceList.Item>
+              );
+            }}
+          />
+        </Card>
+      </Page>
+    </AppProvider>
+  );
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
