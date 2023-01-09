3aec08bb1a2b35dc5725ca83e4286532ca12e90f
benzookapi <jokksk@gmail.com>
2023-01-09 21:03:25 +0900

    #

--- frontend/src/pages/SessionToken.jsx
+++ frontend/src/pages/SessionToken.jsx
@@ -1,36 +1,65 @@
-import { useState, useCallback } from 'react';
-import { Button, Popover, ActionList } from '@shopify/polaris';
+import { useState } from 'react';
+import { useAppBridge } from '@shopify/app-bridge-react';
+import { getSessionToken } from "@shopify/app-bridge-utils";
+import { Page, Card, Layout, Link, Button, Badge, TextContainer } from '@shopify/polaris';
+
+import jwt_decode from "jwt-decode";
 
 // App Bridge Session Token sample
 // See https://shopify.dev/apps/auth/oauth/session-tokens
 function SessionToken() {
-  const [popoverActive, setPopoverActive] = useState(true);
+  const app = useAppBridge();
+  //const redirect = Redirect.create(app);
 
-  const togglePopoverActive = useCallback(
-    () => setPopoverActive((popoverActive) => !popoverActive),
-    [],
-  );
-
-  const activator = (
-    <Button onClick={togglePopoverActive} disclosure>
-      More actions
-    </Button>
-  );
+  const [raw, setRaw] = useState('');
+  const [decoded, setDecoded] = useState('');
 
   return (
-
-    <Popover
-      active={popoverActive}
-      activator={activator}
-      autofocusTarget="first-node"
-      onClose={togglePopoverActive}
-    >
-      <ActionList
-        actionRole="menuitem"
-        items={[{ content: 'Import' }, { content: 'Export' }]}
-      />
-    </Popover>
-
+    <Page title="Getting started with session token authentication">
+      <Card title="Step 1: Get a session token" sectioned={true}>
+        <Layout>
+          <Layout.Section>
+            <Link url="https://shopify.dev/apps/auth/oauth/session-tokens/getting-started#step-1-get-a-session-token" external={true}>Dev. doc</Link>
+          </Layout.Section>
+          <Layout.Section>
+            <Button onClick={() => {
+              getSessionToken(app).then((sessionToken) => {
+                let token = JSON.stringify(sessionToken);
+                let rawToken = '';
+                while (token.length > 0) {
+                  rawToken += `${token.substring(0, 80)}\n`;
+                  token = token.substring(80);
+                }
+                let decodedToken = JSON.stringify(jwt_decode(JSON.stringify(sessionToken)), null, 4);
+                setRaw(rawToken);
+                setDecoded(decodedToken);
+              });
+            }}>
+              Run the code
+            </Button>
+          </Layout.Section>
+          <Layout.Section>
+            <Badge>Raw data:</Badge>
+            <pre>{raw}</pre>
+            <Badge>Decoded data:</Badge>
+            <pre>{decoded}</pre>
+          </Layout.Section>
+        </Layout>
+      </Card>
+      <Card title="Step 2: Authenticate your requests" sectioned={true}>
+        <Layout>
+          <Layout.Section>
+            <Link url="https://shopify.dev/apps/auth/oauth/session-tokens/getting-started#step-2-authenticate-your-requests" external={true}>Dev. doc</Link>
+          </Layout.Section>
+          <Layout.Section>
+            <Button onClick={() => {
+            }}>
+              Run the code
+            </Button>
+          </Layout.Section>
+        </Layout>
+      </Card>
+    </Page>
   );
 }
 
