416ab101d8ced22640fb046bf914d05887415e84
benzookapi <jokksk@gmail.com>
2023-01-20 15:23:16 +0900

    #

--- extensions/my-theme-app-ext/assets/barebone.js
+++ extensions/my-theme-app-ext/assets/barebone.js
@@ -6,3 +6,12 @@ div.setAttribute("style", "border: 2px solid #000000;");
 */
 
 console.log("'assets/barebone.js'");
+
+/* --- Self weorker test --- */
+self.addEventListener("message", (e) => {
+    console.log("Worker test");
+    console.log("Received message: ", e);
+});
+const myWorker = new Worker(self);
+myWorker.postMessage("Hello, this is a barebone app!");
+
