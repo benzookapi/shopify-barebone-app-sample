bae6a21f73be479e20dd365860ac93a91a01538e
benzookapi <jokksk@gmail.com>
2023-01-20 13:38:25 +0900

    #

--- extensions/my-theme-app-ext/assets/barebone.js
+++ extensions/my-theme-app-ext/assets/barebone.js
@@ -1,3 +1,8 @@
-const barebone_ext_func = function (text) {
-    alert(text);
-}
+/* 
+// You cannot write these code to control DOM of blocks which include this JS.
+const div = window.document.createElement("div");
+div.appendChild(window.document.createTextNode("Hi, this is from <b>'assets/barebone.js'</b>. &#128075"));
+div.setAttribute("style", "border: 2px solid #000000;");
+*/
+
+console.log("'assets/barebone.js'");
