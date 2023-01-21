82ef8d80a871dd001d78b7df304a3c265f7f7e5d
benzookapi <jokksk@gmail.com>
2023-01-21 16:40:47 +0900

    #

--- extensions/my-theme-app-ext/assets/barebone.js
+++ extensions/my-theme-app-ext/assets/barebone.js
@@ -1,17 +1,4 @@
-/* 
-// You cannot write these code to control DOM of blocks which include this JS.
-const div = window.document.createElement("div");
-div.appendChild(window.document.createTextNode("Hi, this is from <b>'assets/barebone.js'</b>. &#128075"));
-div.setAttribute("style", "border: 2px solid #000000;");
-*/
+// Note that imported script files by each block are global in theme storefront in browsers, which means 
+// any object are global scopes that cannot conflicts with others
 
 console.log("'assets/barebone.js'");
-
-/* --- Self weorker test --- */
-self.addEventListener("message", (e) => {
-    console.log("Worker test");
-    console.log("Received message: ", e);
-});
-const myWorker = new Worker(self);
-myWorker.postMessage("Hello, this is a barebone app!");
-
