7a3905452016c47c4d193451a8240afbed4dcf87
benzookapi <jokksk@gmail.com>
2023-02-10 19:45:23 +0900

    #

--- extensions/my-web-pixel-ext/src/index.js
+++ extensions/my-web-pixel-ext/src/index.js
@@ -13,39 +13,62 @@ register(({ analytics, browser, settings, init }) => {
   analytics.subscribe('all_events', (event) => {
     console.log(`Web Pixel event received: ${JSON.stringify(event, null, 4)}`);
 
-    const name = event.name;
-
-    const ga4Id = `${settings.ga4Id}`;
-    const ga4Sec = `${settings.ga4Sec}`;
-
     // NOTE THAT IF YOU WANT TO SEND THIS EVENT DATA TO YOUR APP, YOU NEED IT CORS-FREE ACCESSIBLE 
     // BECAUSE THIS WEB WORKER IS A SANDBOX, A STANDALONE BACKEND PROCESS IN BROWSERS 
     // WITH ALL EXTERNAL ACCESS TAKEN FOR CROSS-ORIGIN.
     // See https://developer.mozilla.org/en-US/docs/Glossary/CORS
-    // See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+    // See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API     
 
-    switch (name) {
+    const ga4Url = `https://www.google-analytics.com/mp/collect?measurement_id=${settings.ga4Id}&api_secret=${settings.ga4Sec}`;
+
+    const body = {
+      client_id: `${event.clientId}`,
+      events: [{
+        name: '',
+        params: {
+          ecommerce: {
+            items: []
+          }
+        },
+      }]
+    };
+
+    // See https://shopify.dev/api/pixels/customer-events#checkout_started
+    // THIS EVENT CANNOT BE PASSED BY GA TAG IN THEME APP EXTENSION, ONLY BY WEB PIXEL DURING CHECKOUT PROCESS.
+
+    // Send event data to GA4 (begin_checkout)
+    // See https://developers.google.com/tag-manager/ecommerce-ga4
+    // See https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?hl=ja&client_type=gtag
+
+    let name = null;
+    let items = null;
+    switch (event.name) {
+      case 'product_added_to_cart':
+        name = 'add_to_cart';
+        items = [{
+          "item_name": `${event.data.cartLine.merchandise.product.title}`,
+          "item_id": `${event.data.cartLine.merchandise.product.id}`,
+          "price": event.data.cartLine.merchandise.price.amount,
+          "item_brand": `${event.data.cartLine.merchandise.product.vendor}`,
+          //"item_category": "Apparel",
+          //"item_category2": "Mens",
+          //"item_category3": "Shirts",
+          //"item_category4": "Tshirts",
+          "item_variant": `${event.data.cartLine.merchandise.product.title}`,
+          "item_list_name": `${event.context.document.location.href}`,
+          "item_list_id": `${event.context.document.location.pathname}`,
+          "index": 1,
+          "quantity": event.data.cartLine.quantity
+        }];
+        break;
       case 'checkout_started':
-        // See https://shopify.dev/api/pixels/customer-events#checkout_started
-        // THIS EVENT CANNOT BE PASSED BY GA TAG IN THEME APP EXTENSION, ONLY BY WEB PIXEL DURING CHECKOUT PROCESS.
-
-        // Send event data to GA4 (begin_checkout)
-        // See https://developers.google.com/tag-manager/ecommerce-ga4
-        // See https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?hl=ja&client_type=gtag
-
-        const measurement_id = ga4Id;
-        const api_secret = ga4Sec;
-
-        const items = event.data.checkout.lineItems.map((item, i) => {
+        name = 'begin_checkout';
+        items = event.data.checkout.lineItems.map((item, i) => {
           return {
             "item_name": `${item.title}`,
             "item_id": `${item.id}`,
             "price": item.variant.price.amount,
             "item_brand": `${item.variant.product.vendor}`,
-            //"item_category": "Apparel",
-            //"item_category2": "Mens",
-            //"item_category3": "Shirts",
-            //"item_category4": "Tshirts",
             "item_variant": `${item.variant.product.title}`,
             "item_list_name": `${event.context.document.location.href}`,
             "item_list_id": `${event.context.document.location.pathname}`,
@@ -53,33 +76,31 @@ register(({ analytics, browser, settings, init }) => {
             "quantity": item.quantity
           }
         });
-
-        const body = {
-          client_id: `${event.clientId}`,
-          events: [{
-            name: 'begin_checkout',
-            params: {
-              ecommerce: {
-                items: []
-              }
-            },
-          }]
-        };
-
-        body.events[0].params.ecommerce.items = items;
-
-        consolr.log(`Web Pixel sending 'begin_checkout' of GA4... ${JSON.stringify(body, null, 4)}`);
-
-        fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`, {
-          method: "POST",
-          body: JSON.stringify(body)
-        });
-
-        return;
+        break;
       default:
-        // Other events
-        return;
+        // Other events     
+        break;
     }
+
+    if (name == null) return;
+
+    body.events[0].name = name;
+    body.events[0].params.ecommerce.items = items;
+
+    console.log(`Web Pixel sending 'begin_checkout' of GA4... ${JSON.stringify(body, null, 4)} to ${ga4Url}`);
+    fetch(ga4Url, {
+      method: "POST",
+      headers: {
+        //'Content-Type': 'application/json', // This produce CORS error!
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }).then(res => {
+      console.log(`${JSON.stringify(res, null, 4)}`);
+    }).catch(e => {
+      console.log(`${e}`);
+    });
 
   });
 
