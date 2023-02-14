dd75e2a13465bdda76132def40be08f562eabe25
benzookapi <jokksk@gmail.com>
2023-02-14 16:04:10 +0900

    #

--- frontend/src/pages/PostPurchase.jsx
+++ frontend/src/pages/PostPurchase.jsx
@@ -1,7 +1,8 @@
 import { useState, useCallback } from 'react';
 import { useAppBridge } from '@shopify/app-bridge-react';
 import { authenticatedFetch } from "@shopify/app-bridge-utils";
-import { Page, Card, Layout, Link, List, Badge, Checkbox, TextField, Button, Spinner, Stack } from '@shopify/polaris';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Page, Card, Layout, Link, List, Badge, Button, Spinner, Stack } from '@shopify/polaris';
 
 import { _getShopFromQuery, _getAdminFromShop } from "../utils/my_util";
 
@@ -10,6 +11,7 @@ import { _getShopFromQuery, _getAdminFromShop } from "../utils/my_util";
 // See https://shopify.dev/docs/apps/checkout/post-purchase/getting-started-post-purchase-extension
 function PostPurchase() {
   const app = useAppBridge();
+  const redirect = Redirect.create(app);
 
   const shop = _getShopFromQuery(window);
 
@@ -26,17 +28,23 @@ function PostPurchase() {
           <Layout.Section>
             <List type="number">
               <List.Item>
-                Add <Link url={`https://${_getAdminFromShop(shop)}/settings/custom_data`} external={true}>Metafields</Link> for <Badge status='info'>Products</Badge>
-                in type of <Badge>Single line text</Badge> with namespace and key: <Badge>barebone_app_upsell.product_id</Badge> (<b>Note that the metafield needs 'storefronts' checked = visibleToStorefrontApi: true</b>).
+                <p>
+                  Add <Link url={`https://${_getAdminFromShop(shop)}/settings/custom_data`} external={true}>Metafields</Link> for <Badge status='info'>Products</Badge>
+                  in type of <Badge>Single line text</Badge> with namespace and key: <Badge>barebone_app_upsell.product_id</Badge> (<b>Note that the metafield needs 'storefronts' checked = visibleToStorefrontApi: true</b>).
+                </p>
               </List.Item>
               <List.Item>
-                Set each <Badge>product id (the last number of its detail page URL)</Badge> to each metafield above of <Link url={`https://${_getAdminFromShop(shop)}/products`} external={true}>Products</Link> for
-                those you want to upsell (e.g. "Purchasing Product A with Product B's ID offers the B in post-purchase").
+                <p>
+                  Set each <Badge>product id (the last number of its detail page URL)</Badge> to each metafield above of <Link url={`https://${_getAdminFromShop(shop)}/products`} external={true}>Products</Link> for
+                  those you want to upsell (e.g. "Purchasing Product A with Product B's ID offers the B in post-purchase").
+                </p>
               </List.Item>
               <List.Item>
-                Add another metafield (<Badge>'barebone_app.url'</Badge>) to <Badge status='info'>Shop</Badge> to set this app raw URL (<Badge>https://{window.location.hostname}</Badge>) in it <b>to be accessed from Post-purchase Web Workers </b>
-                using <Link url={`https://shopify.dev/docs/api/admin-graphql/2023-04/mutations/metafieldDefinitionCreate`} external={true}>metafieldDefinitionCreate (visibleToStorefrontApi: true) </Link>
-                and <Link url={`https://shopify.dev/docs/api/admin-graphql/2023-04/mutations/metafieldsSet`} external={true}>metafieldsSet</Link>.
+                <p>
+                  Add another metafield (<Badge>'barebone_app.url'</Badge>) to <Badge status='info'>Shop</Badge> to set this app raw URL (<Badge>https://{window.location.hostname}</Badge>) in it <b>to be accessed from Post-purchase Web Workers </b>
+                  using <Link url={`https://shopify.dev/docs/api/admin-graphql/2023-04/mutations/metafieldDefinitionCreate`} external={true}>metafieldDefinitionCreate (visibleToStorefrontApi: true) </Link>
+                  and <Link url={`https://shopify.dev/docs/api/admin-graphql/2023-04/mutations/metafieldsSet`} external={true}>metafieldsSet</Link>.
+                </p>
                 <Stack spacing="loose">
                   <Button primary onClick={() => {
                     setAccessing(true);
@@ -62,7 +70,9 @@ function PostPurchase() {
                 </Stack>
               </List.Item>
               <List.Item>
-                Select this app as <Badge status='info'>Post-purchase page</Badge> at <Link url={`https://${_getAdminFromShop(shop)}/settings/checkout`} external={true}>Checkout Settings</Link> to enable this Post-purchase.
+                <p>
+                  Select this app as <Badge status='info'>Post-purchase page</Badge> at <Link url={`https://${_getAdminFromShop(shop)}/settings/checkout`} external={true}>Checkout Settings</Link> to enable this Post-purchase.
+                </p>
               </List.Item>
             </List>
           </Layout.Section>
@@ -76,11 +86,24 @@ function PostPurchase() {
           <Layout.Section>
             <List type="number">
               <List.Item>
-                Visit <Link url={`https://${shop}`} external={true}>your theme storefront</Link> to check how your upsells work at your post-purchase.
-                <b>Note that Post-purchase extensions only show up when you use a credit card payment method</b> (i.e. Shopify Payment or Bogus Gateway in general). Any other methods like wallet and
-                3rd party payment app usage don't show that flow.  See <Link url={`https://shopify.dev/docs/apps/checkout/post-purchase#limitations-and-considerations`} external={true}>this limitations</Link>.
+                <p>
+                  Visit <Link url={`https://${shop}`} external={true}>your theme storefront</Link> to check how your upsells work at your post-purchase.
+                  <b>Note that Post-purchase extensions only show up when you use a credit card payment method</b> (i.e. Shopify Payment or Bogus Gateway in general). Any other methods like wallet and
+                  3rd party payment app usage don't show that flow.  See <Link url={`https://shopify.dev/docs/apps/checkout/post-purchase#limitations-and-considerations`} external={true}>this limitations</Link>.
+                </p>
+              </List.Item>
+              <List.Item>
+
               </List.Item>
             </List>
+          </Layout.Section>
+          <Layout.Section>
+            <p>
+              <b>TIPS: </b>This post-purchase communicates with the app raw endpoint over <Link url={`https://shopify.dev/docs/api/checkout-extensions/extension-points#web-platform-globals`} external={true}>CORS</Link> passing shop data with <Link url={`https://shopify.dev/docs/api/checkout-extensions/jwt-specification`} external={true}>JWT validated protection</Link> like this: <Badge>https://{window.location.hostname}/postpurchase?token=eyJhbGciOiJIUzI....</Badge>
+              You can check how the JWT validation protects the raw endpoint copyging the token in the acccessed URL from your browser console to <Link url={`https://jwt.io/`} external={true}>jwt.io</Link> as <Link onClick={() => { redirect.dispatch(Redirect.Action.APP, '/sessiontoken'); }}>
+                Session Token sample
+              </Link> does.
+            </p>
           </Layout.Section>
         </Layout>
       </Card>
