a0ddcaa090123ccfd778a099b86328eda62ad3d9
benzookapi <jokksk@gmail.com>
2024-10-14 15:25:38 +0900

    #

---
+++ extensions/my-checkout-ui-ext-2/src/common.js
@@ -0,0 +1,4 @@
+// This can be used by `Checkout.jsx` and `Checkout.js` both shared with multiple files.
+export const commonFuncExternal = (text) => {
+    console.log(`commonFuncExternal outputs with text: ${text}`);
+};
