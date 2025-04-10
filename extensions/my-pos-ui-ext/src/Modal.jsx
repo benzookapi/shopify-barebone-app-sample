b19179f75f05064a085d22d3405cef98b9090a01
benzookapi <jokksk@gmail.com>
2025-04-10 23:35:18 +0900

    #

--- extensions/my-pos-ui-ext/src/Modal.jsx
+++ extensions/my-pos-ui-ext/src/Modal.jsx
@@ -4,17 +4,46 @@ import {
   Screen,
   Text,
   useScannerDataSubscription,
+  useApi,
   reactExtension,
 } from '@shopify/ui-extensions-react/point-of-sale';
 
 const SmartGridModal = () => {
   const { data } = useScannerDataSubscription();
 
+  const api = useApi();
+
   useEffect(() => {
     if (data) {
-      console.log('Scanner data changed:', data);
+      console.log(`Scanner data changed: ${data}`);
+
       // Call app server side to use Admin API with session token.
-      // https://shopify.dev/docs/api/pos-ui-extensions/2025-01/server-communication     
+      // https://shopify.dev/docs/api/pos-ui-extensions/unstable/server-communication 
+
+      // or add a customer to the current cart with customer id given by barcode or QR to check their online orders, etc.
+      // https://shopify.dev/docs/api/pos-ui-extensions/unstable/apis/cart-api#cartapi-propertydetail-setcustomer
+      api.cart.setCustomer({
+        id: Number(data)
+      }).then(() => {
+        console.log(`api.cart.setCustomer successful with customer id: ${data}`);
+        api.toast.show(`api.cart.setCustomer successful with customer id: ${data}`);
+        api.navigation.dismiss();
+      }).catch((e) => {
+        console.log(`api.cart.setCustomer error: ${JSON.stringify(e)}`);
+        api.toast.show(`api.cart.setCustomer error: ${JSON.stringify(e)}`);
+      });
+
+      // or apply a discount code
+      // https://shopify.dev/docs/api/pos-ui-extensions/unstable/apis/cart-api#cartapi-propertydetail-applycartdiscount
+      /*api.cart.applyCartDiscount('Percentage', 'api.cart.applyCartDiscount()', `${data}`).then(() => {
+        console.log(`api.cart.applyCartDiscount successful with ${data}%`);
+        api.toast.show(`api.cart.applyCartDiscount successful with ${data}%`);
+        api.navigation.dismiss();
+      }).catch((e) => {
+        console.log(`api.cart.applyCartDiscount error: ${JSON.stringify(e)}`);
+        api.toast.show(`api.cart.applyCartDiscount error: ${JSON.stringify(e)}`);
+      });*/
+
     }
   }, [data]);
 
